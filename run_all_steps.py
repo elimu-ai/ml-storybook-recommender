84ff7526f8d80a394523d54a761576d9b1b77840
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2024-09-01 14:29:00 +0700

    build: install pandas
    closes #6

--- run_all_steps.py
+++ run_all_steps.py
@@ -1,18 +1,18 @@
 import os
 
-print('\n*** Step 1. Prepare Data ***')
+print('\n*** Step 1. Prepare Data 🌏 ***')
 os.chdir('step1_prepare')
 print(os.path.basename(__file__), f'os.getcwd(): {os.getcwd()}')
 import step1_prepare.step1_1_download_data
 #import step1_prepare.step1_2_preprocess_data
 #import step1_prepare.step1_3_split_data
 
-print('\n*** Step 2. Train Model ***')
+print('\n*** Step 2. Train Model 🌏🚀 ***')
 os.chdir('../step2_train')
 print(os.path.basename(__file__), f'os.getcwd(): {os.getcwd()}')
 # TODO
 
-print('\n*** Step 3. Make Prediction ***')
+print('\n*** Step 3. Make Prediction 🌏 🚀✨ ***')
 os.chdir('../step3_predict')
 print(os.path.basename(__file__), f'os.getcwd(): {os.getcwd()}')
 # TODO

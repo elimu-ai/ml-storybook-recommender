28bbca3864be8d8708bec90645cf9a39045188e3
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2024-09-01 14:06:57 +0700

    feat: import datasets
    closes #6

---
+++ run_all_steps.py
@@ -0,0 +1,18 @@
+import os
+
+print('\n*** Step 1. Prepare Data ***')
+os.chdir('step1_prepare')
+print(os.path.basename(__file__), 'os.getcwd(): {}'.format(os.getcwd()))
+import step1_prepare.step1_1_download_data
+import step1_prepare.step1_2_preprocess_data
+import step1_prepare.step1_3_split_data
+
+print('\n*** Step 2. Train Model ***')
+os.chdir('../step2_train')
+print(os.path.basename(__file__), 'os.getcwd(): {}'.format(os.getcwd()))
+# TODO
+
+print('\n*** Step 3. Make Prediction ***')
+os.chdir('../step3_predict')
+print(os.path.basename(__file__), 'os.getcwd(): {}'.format(os.getcwd()))
+# TODO

956332d66b5c87930cb20c4adc861adccfef9fcf
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2024-09-01 14:32:56 +0700

    Update run_all_steps.py

--- run_all_steps.py
+++ run_all_steps.py
@@ -8,11 +8,11 @@ import step1_prepare.step1_1_download_data
 #import step1_prepare.step1_3_split_data
 
 print('\n*** Step 2. Train Model 🌏🚀 ***')
-# os.chdir('../step2_train')
-# print(os.path.basename(__file__), f'os.getcwd(): {os.getcwd()}')
+#os.chdir('../step2_train')
+#print(os.path.basename(__file__), f'os.getcwd(): {os.getcwd()}')
 # TODO
 
 print('\n*** Step 3. Make Prediction 🌏 🚀✨ ***')
-# os.chdir('../step3_predict')
-# print(os.path.basename(__file__), f'os.getcwd(): {os.getcwd()}')
+#os.chdir('../step3_predict')
+#print(os.path.basename(__file__), f'os.getcwd(): {os.getcwd()}')
 # TODO
